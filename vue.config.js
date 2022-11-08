4544759dcc22d86314fb1fbe7a3ba5d419273102
Kellie Lynch <kelliedlynch@gmail.com>
2022-11-07 22:12:23 -0600

    Still trying to deploy to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'quasar'
-  ],
-
-  pluginOptions: {
-    quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: false
-    }
-  }
+  publicPath: '/sprinter/'
 })
